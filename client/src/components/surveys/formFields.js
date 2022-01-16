5ce1a9f83c68a99a9b73a7d026c9fedb5810f0b3
Michael Reagan <MichaelLReagan@gmail.com>
2022-01-16 12:28:45 -0500

    Final code from tutorial

---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    label: 'Campaign Title',
+    name: 'title',
+    errorMessage: 'You must provide an email title',
+  },
+  {
+    label: 'Subject Line',
+    name: 'subject',
+    errorMessage: 'You must provide an email subject',
+  },
+  {
+    label: 'Email Body',
+    name: 'body',
+    errorMessage: 'You must provide an email body',
+  },
+  {
+    label: 'Recipient List',
+    name: 'recipients',
+    errorMessage: 'You must provide a recipient list',
+  },
+];
--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const passport = require('passport');
 const keys = require('./config/keys');
 const bodyParser = require('body-parser');
 
-require('./models/user');
+require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
@@ -25,6 +26,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   // make sure express will serve up prodcution assets main.js / main.css
